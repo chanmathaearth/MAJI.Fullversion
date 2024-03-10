c7e6695968d828b552e52fd6254e3167cee7518f
chanmathaearth <107950430+chanmathaearth@users.noreply.github.com>
2024-03-10 22:15:57 +0700

    Add files via upload

---
+++ employee/dashboard/getdata.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "048"; //ตามที่กำหนดให้
+$password = "earth12"; //ตามที่กำหนดให้
+$dbname = "maji";    //ตามที่กำหนดให้
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// คำสั่ง SQL เพื่อหาจำนวนออเดอร์ในแต่ละวัน
+$sql = "SELECT DAYNAME(orderDateTime) AS day_of_week, COUNT(*) AS count 
+        FROM `orders` 
+        WHERE DAYOFWEEK(orderDateTime) BETWEEN 2 AND 7 
+        GROUP BY DAYOFWEEK(orderDateTime)";
+
+// สร้างการเชื่อมต่อกับฐานข้อมูล
+
+// รันคำสั่ง SQL
+$result = mysqli_query($conn, $sql);
+
+// สร้างอาร์เรย์เพื่อเก็บข้อมูล
+$data = array();
+
+// เรียกข้อมูลในรูปแบบของอาร์เรย์และเพิ่มลงในอาร์เรย์
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row['count'];
+}
+
+// ส่งข้อมูลเป็น JSON
+echo json_encode($data);
+// ปิดการเชื่อมต่อ
+mysqli_close($conn);
+?>
---
+++ employee/dashboard/totaltoday.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "048"; //ตามที่กำหนดให้
+$password = "earth12"; //ตามที่กำหนดให้
+$dbname = "maji";    //ตามที่กำหนดให้
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sqlfortoday = "SELECT SUM(amountMoney) AS totalAmount FROM `payment` WHERE DATE(paymentDateTime) = CURDATE()";
+$resultfortoday = mysqli_query($conn, $sqlfortoday);
+$totalAmount = 0;
+
+// เรียกข้อมูลผลลัพธ์และส่งค่ากลับ
+if (mysqli_num_rows($resultfortoday) > 0) {
+    $row = mysqli_fetch_assoc($resultfortoday);
+    echo $row["totalAmount"];
+} else {
+    echo 0;
+}
+
+// ปิดการเชื่อมต่อ
+mysqli_close($conn);
+?>
