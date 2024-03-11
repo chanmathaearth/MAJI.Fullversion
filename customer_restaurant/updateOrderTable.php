dc3f94c2c4f1f2724e89d5e1eb8ab1a141b12c9d
jeena55 <109953515+jeena55@users.noreply.github.com>
2024-03-12 04:11:33 +0700

    Add files via upload

---
+++ customer_restaurant/updateOrderTable.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "maji";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['orderId'])) {
+    $orderId = intval($_POST['orderId']);
+}
+
+if (isset($_SESSION['basket']) && !empty($_SESSION['basket'])) {
+
+    foreach ($_SESSION['basket'] as $index => $item) {
+        $menuId = $item['menuId'];
+        $menuQuantity = $item['menuQuantity'];
+
+        $menuSQL = "INSERT INTO orderDetail (orderId, menuId, menuQuantity, menuStatus) VALUES ($orderId, $menuId, $menuQuantity, 'ได้รับเมนู')";
+        $conn->query($menuSQL);
+    }
+
+    unset($_SESSION['basket']);
+}
+
+$conn->close();
+?>
