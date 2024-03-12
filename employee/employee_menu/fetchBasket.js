a3583d26952c2a37d7d0193a71866461c68a23fa
chanmathaearth <107950430+chanmathaearth@users.noreply.github.com>
2024-03-12 20:57:24 +0700

    Delete employee directory

--- employee/employee_menu/fetchBasket.js
+++
@@ -1,6 +0,0 @@
-fetch('basket.php')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('basket').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching basket:', error));
--- employee/manageTable/employee.js
+++
@@ -1,9 +0,0 @@
-fetch('../topbaremployee.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('topbar').innerHTML = html;
-    });
-
-function loadPage(link) {
-    window.location.href = link;
-}
