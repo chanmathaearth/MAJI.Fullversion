5871ff5ba676f8758843f2da5e579d044fbfaed5
chanmathaearth <107950430+chanmathaearth@users.noreply.github.com>
2024-03-12 20:57:33 +0700

    Delete customer_restaurant directory

--- customer_restaurant/fetchmainNav.js
+++
@@ -1,6 +0,0 @@
-fetch('mainnavbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('mainnavbar').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching mainnavbar:', error));
