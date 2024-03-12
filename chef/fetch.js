f28a35d9a75a1fa9ebbc4e4985daf7c675e6d95d
chanmathaearth <107950430+chanmathaearth@users.noreply.github.com>
2024-03-12 20:57:04 +0700

    Delete chef directory

--- chef/fetch.js
+++
@@ -1,6 +0,0 @@
-fetch('../manageorder/chefnavbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar').innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching navbar:', error));
